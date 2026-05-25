680282d1b06accf254f65974a2abe57f3579d59c
Josh <joshuaepenner@gmail.com>
2026-05-25 11:02:47 -0500

    Create file tree

--- src/popup.js
+++ src/popup.js
@@ -1,67 +1,84 @@
 import { getBookmarks } from "./bookmarks.js";
 import { downloadBookmarks } from "./download.js";
 
-const listElement = document.getElementById("bookmark-list");
-const bookmarks = await getBookmarks();
-let filteredBookmarks = bookmarks;
+let bookmarks = await chrome.bookmarks.getTree();
+if (bookmarks.length == 1) {
+  bookmarks = bookmarks[0].children;
+}
+const treeElement = document.getElementById("bookmark-tree");
 
-const selectAllElement = document.getElementById("check-all");
-const allCheckbox = document.getElementById("all-checkbox");
-allCheckbox.checked = true;
+displayList(treeElement, bookmarks);
 
-selectAllElement.onclick = changeSelectAll;
-document.getElementById("download").onclick = async () => {
-	downloadBookmarks(filteredBookmarks);
-};
+document.querySelectorAll("details").forEach((detail) => {
+  detail.addEventListener("toggle", () => {
+    document.body.style.height = "auto";
+    document.body.style.height = document.body.scrollHeight + "px";
+  });
+});
 
-displayList();
+function displayList(parentElement, bookmarkList) {
+  bookmarkList.forEach((bookmark) => {
+    // For folders I want just the checkbox to be clickable, for leaves I want the whole thing
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.style = "display: flex; gap: 4px; align-items: center";
+    wrapperDiv.id = bookmark.id;
+    parentElement.appendChild(wrapperDiv);
 
-function displayList() {
-	listElement.innerHTML = "";
-	bookmarks.forEach((bookmark) => {
-		const bookmarkElement = document.createElement("li");
-		bookmarkElement.id = bookmark.id;
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.style = "flex-shrink: 0; place-self: start; margin-top: 2px;";
+    checkBox.checked = true;
+    wrapperDiv.appendChild(checkBox);
 
-		const nameElement = document.createElement("label");
-		nameElement.textContent = bookmark.title;
-		nameElement.classList.add("list-label");
+    if (bookmark.children) {
+      const details = document.createElement("details");
+      details.style = "flex-grow: 1; margin-block: 4px";
+      wrapperDiv.appendChild(details);
 
-		const checkBox = document.createElement("input");
-		checkBox.type = "checkbox";
-		checkBox.style = "pointer-events: none;";
-		checkBox.checked = true;
+      const summary = document.createElement("summary");
+      summary.textContent = bookmark.title;
+      details.appendChild(summary);
 
-		bookmarkElement.addEventListener("click", () => {
-			checkBox.checked = !checkBox.checked;
-			refreshFilteredList();
-			updateSelectAll();
-		});
+      const childContainer = document.createElement("div");
+      childContainer.style = "padding-left: 4px;";
+      details.appendChild(childContainer);
 
-		bookmarkElement.appendChild(checkBox);
-		bookmarkElement.appendChild(nameElement);
-		listElement.appendChild(bookmarkElement);
-	});
+      checkBox.addEventListener("click", () => {
+        updateCheckboxes(wrapperDiv);
+      });
+
+      wrapperDiv.classList.add("bookmark-folder");
+
+      displayList(childContainer, bookmark.children);
+    } else {
+      checkBox.style =
+        "pointer-events: none; flex-shrink: 0; place-self: start; margin-top: 2px;";
+
+      const label = document.createElement("label");
+      label.textContent = bookmark.title;
+      label.style = "flex-grow: 1; margin-block: 3px; overflow-x: hidden;";
+
+      wrapperDiv.appendChild(label);
+
+      wrapperDiv.addEventListener("click", () => {
+        checkBox.checked = !checkBox.checked;
+        updateCheckboxes(wrapperDiv);
+      });
+
+      wrapperDiv.classList.add("bookmark-item");
+    }
+  });
 }
 
-function refreshFilteredList() {
-	const checkedElements = Array.from(listElement.children).filter(
-		(child) => child.firstChild.checked,
-	);
-	const checkedIds = checkedElements.map((element) => element.id);
-	filteredBookmarks = bookmarks.filter((bookmark) =>
-		checkedIds.includes(bookmark.id),
-	);
-}
-
-function changeSelectAll() {
-	allCheckbox.checked = !allCheckbox.checked;
-	Array.from(listElement.children).forEach((child) => {
-		child.firstChild.checked = allCheckbox.checked;
-	});
-}
-
-function updateSelectAll() {
-	allCheckbox.checked =
-		Array.from(listElement.children).filter((child) => child.firstChild.checked)
-			.length > 0;
+function updateCheckboxes(element) {
+  // TODO propogate up the folder select status
+  const isChecked = element.querySelector("input").checked;
+  if (element.className.includes("bookmark-folder")) {
+    const childBookmarks = element.querySelectorAll(
+      ".bookmark-item, .bookmark-folder",
+    );
+    childBookmarks.forEach((bookmarkElement) => {
+      bookmarkElement.querySelector("input").checked = isChecked;
+    });
+  }
 }
